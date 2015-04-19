3e56ac60909c5d638d71324717aad4f08f7fa485
Jerry Vongkhamsay <jvongkhamsay@hotmail.com>
2015-04-19 18:36:40 +0000

    Completed

--- Admin/logout.php
+++ Admin/logout.php
@@ -4,5 +4,5 @@ session_destroy();
 include('../includes/_header.php');
 echo "you have been logged out! <br><a href='login.php'>Login Page</a> ";
 include('../includes/_footer.php');
-
+ 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
      include 'includes/_header.php';   
     
 ?>
